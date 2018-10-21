876aa2e26b7fe5e36705041a24ce0cc21b715c13
Alejandro Tejada Rodríguez <thertejada@gmail.com>
2018-10-21 14:29:09 +0200

    Random entre 10 y 50

--- src/alejandrotejada_adivino/AlejandroTejada_Adivino.java
+++ src/alejandrotejada_adivino/AlejandroTejada_Adivino.java
@@ -6,7 +6,7 @@ public class AlejandroTejada_Adivino {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) (Math.random() * (10-51) + 51);20
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
